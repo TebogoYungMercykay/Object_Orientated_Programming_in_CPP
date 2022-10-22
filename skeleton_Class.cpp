4afb7c6728f4c892d2e7c7c2fe84498b5c97e971
Selepe Sello <sbkskhalo.kq@gmail.com>
2022-10-22 12:33:02 +0200

    Create skeleton_Class.cpp

---
+++ skeleton_Class.cpp
@@ -0,0 +1,39 @@
+// C++ program to demonstrate function
+// declaration outside class
+#include <iostream>
+
+using namespace std;
+class class_Name
+{
+    private:
+    protected:
+    public:
+        string name;
+        int id;   
+        // printname is not defined inline
+        void printname();
+        // printid is defined inline
+        void printid()
+        {
+            cout << "User id is: " << id;
+        }
+};
+ 
+// Definition of printname using scope resolution operator ::
+void class_Name::printname()
+{
+    cout << "User is: " << name;
+}
+// Main function
+int main()
+{
+    class_Name obj1;
+    obj1.name = "xyz";
+    obj1.id=15;
+    // call printname()
+    obj1.printname();
+    cout << endl;
+    // call printid()
+    obj1.printid();
+    return 0;
+}
