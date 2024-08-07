0266e4aeb19ca0c6b1eda26169b2743364ade308
tebogoyungmercykay <u20748052@tuks.co.za>
2024-08-06 20:47:03 -0400

    9 - (update) Polymorpism for Numbers

---
+++ src/polymorphism/implementation-9/IsDivisible.cpp
@@ -0,0 +1,22 @@
+#include "IsDivisible.h"
+int IsDivisible::numAliveObjects = 0;
+
+IsDivisible::IsDivisible(int Value) : ValueDependantTester(Value) {
+  this->numAliveObjects++;
+}
+IsDivisible::~IsDivisible() { this->numAliveObjects--; }
+bool IsDivisible::evaluate(int val) {
+  if (this->value != 0) {
+    if (val % this->value == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  return false;
+}
+NumberTester *IsDivisible::clone() {
+  IsDivisible *ptr = new IsDivisible(this->value);
+  return ptr;
+}
+int IsDivisible::getNumAliveObjects() { return numAliveObjects; }
