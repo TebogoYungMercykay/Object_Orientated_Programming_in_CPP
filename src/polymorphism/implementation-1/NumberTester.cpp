72e9e37c40c7e2a7c0906b6ca83bc6228fea5736
tebogoyungmercykay <u20748052@tuks.co.za>
2024-08-16 20:45:25 -0400

    1- (update) Polymorpism for Numbers

---
+++ src/polymorphism/implementation-1/NumberTester.cpp
@@ -0,0 +1,8 @@
+#include "NumberTester.h"
+int NumberTester::numAliveObjects = 0;
+
+NumberTester::NumberTester() { NumberTester::numAliveObjects += 1; }
+NumberTester::~NumberTester() { NumberTester::numAliveObjects -= 1; }
+bool NumberTester::evaluate(int val) { return false; }
+NumberTester *NumberTester::clone() { return NULL; }
+int NumberTester::getNumAliveObjects() { return numAliveObjects; }
