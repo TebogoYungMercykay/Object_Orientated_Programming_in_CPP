edba3c8108e4b62b91e4b2577b56b08b34cb98ba
tebogoyungmercykay <u20748052@tuks.co.za>
2024-08-23 20:43:03 -0400

    (update) Calendar Implemntation Using Linked Lists

---
+++ src/singly-linked-list/Event.h
@@ -0,0 +1,23 @@
+#ifndef EVENT_H
+#define EVENT_H
+
+#include "Utils.h"
+#include <string>
+
+using namespace std;
+
+class Event {
+public:
+  long int start;
+  long int end;
+  int id;
+  string type;
+  Event *next;
+  ~Event();
+  Event(long int start, long int end, int id, string type);
+  friend ostream &operator<<(ostream &out, Event &event);
+
+protected:
+private:
+};
+#endif // EVENT_H
