0df8908f2837f60ae47edd36a1b2acae3e32b0cc
tebogoyungmercykay <u20748052@tuks.co.za>
2024-08-23 20:36:46 -0400

    (update) C++ Game Using Lists and Recursion

---
+++ src/linkedlists-and-recursion/Lamp.h
@@ -0,0 +1,10 @@
+#ifndef LAMP_H
+#define LAMP_H
+#include "Object.h"
+
+class Lamp : public Object {
+public:
+  Lamp(int x, int y);
+  void update();
+};
+#endif
---
+++ src/linkedlists-and-recursion/Object.h
@@ -0,0 +1,35 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+#include <sstream>
+
+class Object {
+private:
+protected:
+  char icon;
+  int xPos;
+  int yPos;
+  // Part 2
+  bool solid;
+  bool lit;
+
+public:
+  Object *nextHoriz;
+  Object *nextVert;
+  Object *prevVert;
+  Object *prevHoriz;
+  Object *above;
+  Object *below;
+  Object(int x, int y);
+  char getIcon();
+  int getCoord(bool dimension);
+  Object *getNext(bool dimension);
+  Object *getPrev(bool dimension);
+  void setNext(Object *obj, bool dimension);
+  void setPrev(Object *obj, bool dimension);
+  // Part 2
+  void update();
+  bool isSolid();
+  void updateLight(char direction, int intensity);
+  void interact();
+};
+#endif
