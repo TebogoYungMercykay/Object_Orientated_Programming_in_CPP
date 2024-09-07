77955745429ba16e3c5ce65ba1204d9d4c0fe69d
tebogoyungmercykay <u20748052@tuks.co.za>
2024-09-06 20:34:09 -0400

    (i5 update) Matrix Arithmetic and Operator Overloading

---
+++ src/matrices/matrix-arithmetic/implementation-5/arithmetic.cpp
@@ -0,0 +1,11 @@
+#include "arithmetic.h"
+
+Arithmetic::Arithmetic() {}
+
+Arithmetic::Arithmetic(const Arithmetic &rhs) {}
+
+Arithmetic::~Arithmetic() {}
+
+void Arithmetic::print() {} // pure virtual
+
+void Arithmetic::readFile(istream &infile) {} // pure virtual
