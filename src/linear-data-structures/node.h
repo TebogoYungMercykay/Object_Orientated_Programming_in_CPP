49a200b36878e51a07f94ab02240aa78c99de2b9
tebogoyungmercykay <u20748052@tuks.co.za>
2024-09-06 20:32:39 -0400

    (update) Testing Linear DS, Implementation & Formatting

---
+++ src/linear-data-structures/node.h
@@ -0,0 +1,23 @@
+//
+// Created by DitFred on 10/29/2020.
+//
+
+#ifndef ASSIGNMENT_4_NODE_H
+#define ASSIGNMENT_4_NODE_H
+
+template <class T> class Node {
+public:
+  Node(T data, Node<T> *n = 0) {
+    element = data;
+    next = n;
+  }
+
+  ~Node() { next = 0; }
+
+  // The element stored in the node
+  T element;
+  // The next node in the list
+  Node<T> *next;
+};
+
+#endif // ASSIGNMENT_4_NODE_H
