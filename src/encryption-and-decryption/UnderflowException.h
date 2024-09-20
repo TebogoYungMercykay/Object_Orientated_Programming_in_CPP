7596b3c71eef1da26b5f2027fbf6c8a7f10027ca
tebogoyungmercykay <u20748052@tuks.co.za>
2024-09-19 20:27:35 -0400

    (update) Encryption & Decryption Algorithms Fix & Formatting

---
+++ src/encryption-and-decryption/UnderflowException.h
@@ -0,0 +1,9 @@
+#ifndef UNDERFLOWEXCEPTION_H
+#define UNDERFLOWEXCEPTION_H
+#include "BabushkaException.h"
+
+class UnderflowException : public BabushkaException {
+public:
+  virtual void printMessage();
+};
+#endif
