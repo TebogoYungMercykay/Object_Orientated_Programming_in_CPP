91e4bdad6422fcdb9e1fb9e97510b4f44a4554ae
tebogoyungmercykay <u20748052@tuks.co.za>
2024-10-11 20:09:18 -0400

    (Update I2) Calculator Simulator Fix and Formating

---
+++ src/calculator-simulation-using-stacks/implementation-2/Node.cpp
@@ -0,0 +1,9 @@
+#include "Node.h"
+
+template <typename T> Node<T>::Node(T data) {
+  this->data = data;
+  this->next = NULL;
+}
+template <typename T> T Node<T>::getData() { return this->data; }
+template <typename T> Node<T> *Node<T>::getNext() { return this->next; }
+template <typename T> void Node<T>::setNext(Node<T> *ptr) { this->next = ptr; }
